fb50d241dcfa650025cae0b1674121709e29dded
marcolopez16 <marcoolopez16@gmail.com>
2025-02-24 12:14:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def set_background(image_file):
     st.markdown(css, unsafe_allow_html=True)
 
 # Llamar a la función con el nombre del archivo de la imagen
-set_background("fondo.jpg")
+set_background("C:\Users\marco\OneDrive\Escritorio\fondo.jpg")
 
 st.title("Análisis de Acciones")
 
